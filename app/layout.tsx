f7ea7f850d1e8c654aaae3b8167a220040204933
wendimust <wendimusit@gmail.com>
2026-01-16 00:58:49 +0300

    chore: use the migarated bottom nav bar on the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import BottomNav from "../components/bottom-nav";
 import { geistSans, geistMono } from "./ui/fonts";
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <BottomNav />
       </body>
     </html>
   );
